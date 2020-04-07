c9fc2f172b86de873a8a86f6128cb45d9001e61b
BauerrDeveloper <42820915+BauerrDeveloper@users.noreply.github.com>
2020-04-07 10:34:11 -0300

    Commit realizado pelo site.

---
+++ TesteArrays.java
@@ -0,0 +1,12 @@
+package br.com.rodrigo.arrays;
+
+public class TesteArrays {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Testando a implementação do github.");
+		System.out.println("escremento.");
+		System.out.println("/parte6-arrays/src/br/com/rodrigo/arrays/TesteArrays.java");
+
+	}
+}
