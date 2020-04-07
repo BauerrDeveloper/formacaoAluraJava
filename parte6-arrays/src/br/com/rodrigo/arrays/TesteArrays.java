bfccf7e07c129bf2ddb2b941360b825e44f62776
BauerrDeveloper <ro.bauerr@gmail.com>
2020-04-07 10:23:48 -0300

    Testando o escremento.

--- parte6-arrays/src/br/com/rodrigo/arrays/TesteArrays.java
+++ parte6-arrays/src/br/com/rodrigo/arrays/TesteArrays.java
@@ -5,6 +5,7 @@ public class TesteArrays {
 	public static void main(String[] args) {
 		
 		System.out.println("Testado a implementação do git.");
+		System.out.println("Outro teste de escremento.");
 
 	}
 
