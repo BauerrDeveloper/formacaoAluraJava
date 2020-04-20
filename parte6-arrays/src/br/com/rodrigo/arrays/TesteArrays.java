26eee1dca0973bac5a2d5a13afee65f830760824
Rodrigo Bauernfeind Bauernfeind <rodrigo_pb9@hotmail.com>
2020-04-20 15:43:26 -0300

    Commit da parte 6 aula 01

--- parte6-arrays/src/br/com/rodrigo/arrays/TesteArrays.java
+++ parte6-arrays/src/br/com/rodrigo/arrays/TesteArrays.java
@@ -4,10 +4,22 @@ public class TesteArrays {
 
 	public static void main(String[] args) {
 		
-		System.out.println("bauesfild");
-		System.out.println("");
-		System.out.println("testando o git.");
+		int[] idades = new int[5];//inicializa com os valores padrões. No caso int = 0;
 		
-	}
 
+		for (int i = 0; i < idades.length; i++) {
+			idades[i] = i *i;
+			}
+		
+		for (int i = 0; i < idades.length; i++) {
+			System.out.println(idades[i]);
+		}
+	}
+	
 }
+
+		
+		
+	
+
+
