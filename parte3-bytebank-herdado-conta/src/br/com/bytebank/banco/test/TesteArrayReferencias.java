1acde44280de02f9f10ee5ee7a619d149e8450ae
Rodrigo Bauernfeind Bauernfeind <rodrigo_pb9@hotmail.com>
2020-05-06 18:05:10 -0300

    Testando Arrays

--- parte3-bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ parte3-bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,29 +1,40 @@
 package br.com.bytebank.banco.test;
 
-
-
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
-	public static void main(String[] args) {
-		
-		ContaCorrente[] contas = new ContaCorrente[5];
-		
-		ContaCorrente cc1 = new ContaCorrente(22, 11);
-		ContaCorrente cc2 = new ContaCorrente(22, 22);
-		
-		contas[0] = cc1;
-		contas[1] = cc2;
-		
-		System.out.println("Na posição 0 do meu array: " + contas[0].toString());
-		System.out.println(contas[1].toString());
-		System.out.println(contas[1].getNumero());
-		
-		ContaCorrente ref = contas[1];
-		System.out.println(cc2.getNumero());
-		System.out.println(ref.getNumero());
-		
-		
-	}
+	
+
+        public static void main(String[] args, ContaPoupanca[] contas) {
+
+        //int[] idades = new int[5];
+        Object[] referencias = new Object[5];
+
+        ContaCorrente cc1 = new ContaCorrente(22, 11);
+        referencias[0] = cc1;
+
+        ContaPoupanca cc2 = new ContaPoupanca(22, 22);
+        referencias[1] = cc2;
+
+        Cliente cliente = new Cliente();
+        referencias[2] = cliente;
+
+        //System.out.println(cc2.getNumero());
+
+        //Object referenciaGenerica = contas[1];
+
+        //System.out.println( referenciaGenerica.getNumero() );
+
+       
+		ContaPoupanca ref = (ContaPoupanca) contas[1]; //type cast
+        System.out.println(cc2.getNumero());
+        System.out.println(ref.getNumero());
+
+        }
+
 }
+
